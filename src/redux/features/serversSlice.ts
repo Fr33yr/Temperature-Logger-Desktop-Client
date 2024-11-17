72355a6a64a90615fc4d7ff828035481516a73e1
fr33yr <fr33yr@gmail.com>
2024-11-17 16:45:52 -0300

    servers reducers

--- src/redux/features/serversSlice.ts
+++ src/redux/features/serversSlice.ts
@@ -1,11 +1,16 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export const initialServersState = {
-  value: {
-    selectedServer: {},
-    servers: [],
-  },
-};
+interface IServer {
+  name: string;
+  url: string;
+}
+
+interface IServersInitialState {
+  selectedServer: IServer;
+  servers: IServer[];
+}
+
+export const initialServersState = {} as IServersInitialState;
 
 export const servers = createSlice({
   name: "servers",
@@ -13,6 +18,39 @@ export const servers = createSlice({
   reducers: {
     reset: () => {
       return initialServersState;
+    },
+    selectServer: (state, action: PayloadAction<IServer>) => {
+      const { name, url } = action.payload;
+      return {
+        ...state,
+        selectedServer: {
+          name,
+          url,
+        },
+      };
+    },
+    addServer: (state, action: PayloadAction<IServer>) => {
+      return {
+        ...state,
+        servers: [...state.servers, action.payload],
+      };
+    },
+    deleteServer: (state, action: PayloadAction<String>) => {
+      if (action.payload == state.selectedServer.url) {
+        return {
+          ...state,
+          selectedServer: {
+            name: "",
+            url: "",
+          },
+          servers: state.servers.filter((item) => item.url !== action.payload),
+        };
+      } else {
+        return {
+          ...state,
+          servers: state.servers.filter((item) => item.url !== action.payload),
+        };
+      }
     },
   },
 });
