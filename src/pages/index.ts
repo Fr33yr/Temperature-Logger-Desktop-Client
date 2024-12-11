b0203080e1196fd72ae779a80aa208a9fcd63c2c
fr33yr <fr33yr@gmail.com>
2024-12-11 19:54:49 -0300

    logs component

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,3 @@
 export { default as Config } from './Config';
-export { default as Logs } from './logs/Logs';
 export { default as Servers } from './servers/Servers';
+export { default as Logs } from './templogs/Logs';
---
+++ src/pages/templogs/Logs.tsx
@@ -0,0 +1,168 @@
+import { fetch } from "@tauri-apps/plugin-http";
+import { debug, error as logError } from "@tauri-apps/plugin-log";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+} from "chart.js";
+
+import "./Logs.css";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxhook";
+import { addSensorData, setCurrentLog } from "../../redux/features/logsSlice";
+import { ITempLog, LogTimeRange } from "../../interfaces/logs";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement
+);
+
+export default function Logs() {
+  const selectedServer = useAppSelector((state) => state.serversReducer);
+  const { currentLog } = useAppSelector((state) => state.logsReducer); // the selected list of log
+  const dispatch = useAppDispatch();
+  const Data: ITempLog[] = [
+    {
+      id: 2346,
+      name: "sensor_1",
+      temperature: -27.81,
+      created_at: "2024-10-23T11:47:45.128Z",
+    },
+    {
+      id: 2345,
+      name: "sensor_0",
+      temperature: 7.69,
+      created_at: "2024-10-23T11:47:44.910Z",
+    },
+    {
+      id: 2347,
+      name: "sensor_1",
+      temperature: -28.81,
+      created_at: "2024-10-23T11:47:45.128Z",
+    },
+    {
+      id: 2344,
+      name: "sensor_0",
+      temperature: 5.69,
+      created_at: "2024-10-23T11:47:44.910Z",
+    },
+    {
+      id: 2348,
+      name: "sensor_1",
+      temperature: -28.81,
+      created_at: "2024-10-23T11:47:45.128Z",
+    },
+    {
+      id: 2349,
+      name: "sensor_0",
+      temperature: 5.69,
+      created_at: "2024-10-23T11:47:44.910Z",
+    },
+  ];
+
+  const uniqueNames = Data.reduce<string[]>((accumulator, currentValue) => {
+    if (!accumulator.includes(currentValue.name)) {
+      accumulator.push(currentValue.name);
+    }
+    return accumulator;
+  }, []);
+
+  const colors = ["#f3ba2f", "#2a71d0", "#f3ba2f"];
+
+  const DataSets = uniqueNames.map((element, index) => {
+    return {
+      label: element,
+      data: Data.filter((log) => log.name == element).map((d) => d.temperature),
+      backgroundColor: colors[index],
+      borderColor: "black",
+      borderWidth: 1,
+    };
+  });
+
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => {
+      const date = new Date(data.created_at);
+      const formattedDate = `${date
+        .getHours()
+        .toString()
+        .padStart(2, "0")}:${date
+        .getMinutes()
+        .toString()
+        .padStart(2, "0")} ${date.getDate().toString().padStart(2, "0")}/${(
+        date.getMonth() + 1
+      )
+        .toString()
+        .padStart(2, "0")}`;
+
+      return formattedDate;
+    }),
+    datasets: DataSets,
+  });
+
+  useEffect(()=>{
+    if(currentLog && currentLog.length){
+
+    }
+  },[currentLog])
+  
+
+  return (
+    <div className="logs">
+      <h2 className="title">Logs</h2>
+      <span>{currentLog && JSON.stringify(currentLog)}</span>
+      <Line
+        data={chartData}
+        options={{
+          plugins: {
+            title: { display: true, text: "..." },
+            legend: { display: false },
+          },
+        }}
+      />
+      <div className="logsButtons">
+        <button onClick={() => fetchLogsData(LogTimeRange.Week)}>Week</button>
+        <button onClick={() => fetchLogsData(LogTimeRange.Day)}>Day</button>
+        <button onClick={() => fetchLogsData(LogTimeRange.Hour)}>Hour</button>
+        <button onClick={() => fetchLogsData(LogTimeRange.Live)}>Live</button>
+      </div>
+    </div>
+  );
+
+  async function fetchLogsData(logTimeFrame: LogTimeRange) {
+    try {
+      const response = await fetch(
+        `${selectedServer.selectedServer.url}templogs`,
+        {
+          method: "GET",
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        debug("response ok!");
+        dispatch(addSensorData({ flag: logTimeFrame, logs: data }));
+        if (data && Array.isArray(data) && data.length > 0) {
+          dispatch(addSensorData({ flag: logTimeFrame, logs: data }));
+          dispatch(setCurrentLog(logTimeFrame))
+        } else {
+          debug("No valid data received");
+        }
+      } else {
+        logError(`Failed to fetch: ${response.status} ${response.statusText}`);
+      }
+    } catch (error) {
+      logError(`Error fetching data ${error}`);
+    }
+  }
+}
